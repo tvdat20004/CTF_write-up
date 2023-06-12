4496797ee9aeacf2cb3ed769087e53ea5c70e9f2
Thái Vĩnh Đạt <117071011+tvdat20004@users.noreply.github.com>
2023-06-13 02:09:46 +0700

    Update solve.py

--- SEETF 2023/baby_RC4/solve.py
+++ SEETF 2023/baby_RC4/solve.py
@@ -3,16 +3,6 @@ from argparse import ArgumentParser
 import sys
 
 def main():
-    # description = """ 
-    # Decrypts RC4 ciphertexts with static keys.
-    # The arguments need to be files, output is delivered via stdout.
-    # """
-    # parser = ArgumentParser(description=description)
-    # parser.add_argument("knownPlaintext", help="Known Plaintext")
-    # parser.add_argument("knownCiphertext", help="Ciphertext derived from knownPlaintext")
-    # parser.add_argument("unknownCiphertext", help="Ciphertext you want to decrypt")
-    # args = parser.parse_args()
-
     knownPlaintext = b'a'*36
     knownCiphertext = bytes.fromhex('a5c237b6102db668ce467579c702d5af4bec7e7d4c0831e3707438a6a3c818d019d555fc')
     unknownCiphertext = bytes.fromhex('b99665ef4329b168cc1d672dd51081b719e640286e1b0fb124403cb59ddb3cc74bda4fd85dfc')
