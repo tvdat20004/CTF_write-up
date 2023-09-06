7723fb4e6d8370e18c5b775bd5fb41dce70b1899
Thái Vĩnh Đạt <117071011+tvdat20004@users.noreply.github.com>
2023-09-06 21:13:03 +0700

    Update solve.py

--- DownUnderCTF2023/flag_art/solve.py
+++ DownUnderCTF2023/flag_art/solve.py
@@ -17,3 +17,4 @@ for i in range(0,len(template),4):
 	idx = crt(mod, [palette[template[i]], palette[template[i+1]], palette[template[i+2]], palette[template[i+3]]])[0]
 	flag += chr(idx)
 print(flag)
+# Congratulations on solving this challenge! The mask has 900 X's so here are some random words to make the message long enough. Your flag is: DUCTF{r3c0nstruct10n_0f_fl4g_fr0m_fl4g_4r7_by_l00kup_t4bl3_0r_ch1n3s3_r3m41nd3r1ng?}
